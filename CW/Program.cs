48a75ef79fc87dc5d5741b69040f214a608f4901
Siberprost <Skripnichencko.nikita@yandex.ru>
2022-09-08 01:29:21 +0300

    upgrate version- can write your words

--- CW/Program.cs
+++ CW/Program.cs
@@ -1,22 +1,16 @@
-﻿string[] array =
-{
-    "git",
-    "zoms",
-    "rus",
-    "cucumber",
-    "luck",
-    "111"
-};
- 
-var result = new string[array.Length];
-var realSize = 0;
-foreach (var value in array)
-{
-    if (value.Length <= 3)
-    {
-        result[realSize] = value;
-        realSize++;
-    }
-}
- 
-Console.WriteLine(string.Join(Environment.NewLine, result, 0, realSize));
+﻿ string[] M;
+            Console.WriteLine("Введите массив строк через пробел");
+            string stroka = Console.ReadLine();
+            M = stroka.Split(' ');
+            var result = new string[M .Length];
+            var realSize = 0;
+            foreach (var value in M )
+            {
+                if (value.Length <= 3)
+                {
+                    result[realSize] = value;
+                    realSize++;
+                }
+            }
+            Console.WriteLine(string.Join(Environment.NewLine, result, 0, realSize));
+            Console.ReadKey(true);
